180dd61841cef4aa281f9c340d9e770644fb21c7
German Vega <German.vega@imag.fr>
2011-04-05 08:09:09 +0000

    Perform test in its own thread to avoid blocking iPojo

--- fr.imag.adele.appam.test.create/src/main/java/fr/imag/adele/appam/test/create/CreateAppliTest.java
+++ fr.imag.adele.appam.test.create/src/main/java/fr/imag/adele/appam/test/create/CreateAppliTest.java
@@ -1,20 +1,40 @@
 package fr.imag.adele.appam.test.create;
 
+import java.io.File;
+import java.net.MalformedURLException;
+
 import fr.imag.adele.apam.apamAPI.Apam;
 import fr.imag.adele.apam.apamAPI.Application;
 
-public class CreateAppliTest {
+public class CreateAppliTest implements Runnable {
     // iPOJO injected
     Apam apam;
+    
+    /**
+     * The test is performed in its own thread triggered when the component is activated
+     */
+    public void run() {
+		try {
+			Application appli = apam.createAppli("monAppliADeployer", null,"DependencyTest", new File("C:/Users/vega/workspace/fr.imag.adele.appam.test.dependency/target/test.dependency-1.0.0.jar").toURI().toURL(), "bundle",null, null);
+	        apam.dumpApam();
+	        appli.execute(null);
+	        apam.dumpApam();
+	        
+	        appli = apam.createAppli("monAppliDejaDeploye", null, "DependencyTest", null, null, null);
+	        apam.dumpApam();
+	        appli.execute(null);
+	        apam.dumpApam();
+	        
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+        
+    	
+    }
 
     @SuppressWarnings("unused")
     private void start() {
-
-        Application appli = apam.createAppli("monAppli", null, "DependencyTest", null, null, null);
-        apam.dumpApam();
-        appli.execute(null);
-        apam.dumpApam();
-
+    	new Thread(this,"APAM test").start();
     }
 
 }
