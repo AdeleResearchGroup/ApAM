ec1356ac821a525f26f76a0309eff6131ceb2c20
Jacky Estublier <Jacky.Estublier@imag.fr>
2011-07-28 11:31:43 +0000

    Constantes des composites

--- APAMProject/APAM/src/main/java/fr/imag/adele/apam/CST.java
+++ APAMProject/APAM/src/main/java/fr/imag/adele/apam/CST.java
@@ -20,55 +20,65 @@ public class CST {
 
     // Constants
     // value object : address of the iPOJO apam dependency handler
-    public static final String         A_DEPHANDLER     = "ApamDependencyHandler";
-    public static final String         A_APAMSPECNAME   = "ApamSpecName";
-    public static final String         A_APAMIMPLNAME   = "ApamImplName";
+    public static final String         A_DEPHANDLER                           = "ApamDependencyHandler";
+    public static final String         A_APAMSPECNAME                         = "ApamSpecName";
+    public static final String         A_APAMIMPLNAME                         = "ApamImplName";
 
     // indicate in which scope this object is visible. Instance have the visibility of its implementaiton
-    public static final String         A_SCOPE          = "SCOPE";
+    public static final String         A_SCOPE                                = "SCOPE";
     // visible everywhere
-    public static final String         V_GLOBAL         = "GLOBAL";
+    public static final String         V_GLOBAL                               = "GLOBAL";
     // visible in the current appli only
-    public static final String         V_APPLI          = "APPLI";
+    public static final String         V_APPLI                                = "APPLI";
     // visible in the current composite and on composites that depend on the current composite
-    public static final String         V_COMPOSITE      = "COMPOSITE";
+    public static final String         V_COMPOSITE                            = "COMPOSITE";
     // visible in the current composite only
-    public static final String         V_LOCAL          = "LOCAL";
+    public static final String         V_LOCAL                                = "LOCAL";
 
     // multiple on a group head indicates if more than one resolution is allowed in the scope
-    public static final String         A_MULTIPLE       = "MULTIPLE";
+    public static final String         A_MULTIPLE                             = "MULTIPLE";
     // remotable indicates that the instance can be used from a remote machine
-    public static final String         A_REMOTABLE      = "REMOTABLE";
+    public static final String         A_REMOTABLE                            = "REMOTABLE";
     // shared on an implementation indicates if its instances can have more than one incoming wire
-    public static final String         A_SHARED         = "SHARED";
+    public static final String         A_SHARED                               = "SHARED";
     //for boolean attributes
-    public static final String         V_TRUE           = "TRUE";
-    public static final String         V_FALSE          = "FALSE";
+    public static final String         V_TRUE                                 = "TRUE";
+    public static final String         V_FALSE                                = "FALSE";
 
     // Managers
-    public static final String         APAMMAN          = "APAMMAN";
-    public static final String         SAMMAN           = "SAMMAN";
-    public static final String         CONFMAN          = "CONFMAN";
-    public static final String         DYNAMAN          = "DYNAMAN";
-    public static final String         DISTRIMAN        = "DISTRIMAN";
-    public static final String         OBRMAN           = "OBRMAN";
+    public static final String         APAMMAN                                = "APAMMAN";
+    public static final String         SAMMAN                                 = "SAMMAN";
+    public static final String         CONFMAN                                = "CONFMAN";
+    public static final String         DYNAMAN                                = "DYNAMAN";
+    public static final String         DISTRIMAN                              = "DISTRIMAN";
+    public static final String         OBRMAN                                 = "OBRMAN";
+
+    //Property names of composites
+    //Value boolean
+    public static final String         PROPERTY_COMPOSITE                     = "apam-composite";
+    //String
+    public static final String         PROPERTY_COMPOSITE_MAIN_IMPLEMENTATION = "apam-implementation";
+    //String
+    public static final String         PROPERTY_COMPOSITE_MAIN_SPECIFICATION  = "apam-specification";
+    // List<ManagerModel>
+    public static final String         PROPERTY_COMPOSITE_MODELS              = "apam-models";
 
     // The entry point in the ASM : its brokers
-    public static ASMSpecBroker        ASMSpecBroker    = null;
-    public static ASMImplBroker        ASMImplBroker    = null;
-    public static ASMInstBroker        ASMInstBroker    = null;
+    public static ASMSpecBroker        ASMSpecBroker                          = null;
+    public static ASMImplBroker        ASMImplBroker                          = null;
+    public static ASMInstBroker        ASMInstBroker                          = null;
 
     // The entry point in SAM : its brokers
-    public static SpecificationBroker  SAMSpecBroker    = null;
-    public static ImplementationBroker SAMImplBroker    = null;
-    public static InstanceBroker       SAMInstBroker    = null;
-    public static DeploymentUnitBroker SAMDUBroker      = null;
+    public static SpecificationBroker  SAMSpecBroker                          = null;
+    public static ImplementationBroker SAMImplBroker                          = null;
+    public static InstanceBroker       SAMInstBroker                          = null;
+    public static DeploymentUnitBroker SAMDUBroker                            = null;
 
     // the Apam entry point.
-    public static APAMImpl             apam             = null;
+    public static APAMImpl             apam                                   = null;
 
     // the implementation event handler. It installs an implem and waits for its apparition in SAM
-    public static SamImplEventHandler  implEventHandler = null;
+    public static SamImplEventHandler  implEventHandler                       = null;
 
     public CST(APAMImpl theApam) {
         try {
