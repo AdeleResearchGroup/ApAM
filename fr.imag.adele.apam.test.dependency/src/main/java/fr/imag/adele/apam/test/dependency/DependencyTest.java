623012fb7e11477c624b6cc3cfdfa0cecc260986
Jacky Estublier <Jacky.Estublier@imag.fr>
2011-09-21 07:30:10 +0000

    Adaptation aux nouvelles interfaces

--- fr.imag.adele.apam.test.dependency/src/main/java/fr/imag/adele/apam/test/dependency/DependencyTest.java
+++ fr.imag.adele.apam.test.dependency/src/main/java/fr/imag/adele/apam/test/dependency/DependencyTest.java
@@ -2,15 +2,26 @@ package fr.imag.adele.apam.test.dependency;
 
 import java.util.List;
 
-public class DependencyTest implements DependencyTestProvidedInterface {
+import fr.imag.adele.apam.apamAPI.ASMInst;
+import fr.imag.adele.apam.apamAPI.ApamComponent;
 
-	private List<DependencyTestInterface> dependencies;
+public class DependencyTest implements DependencyTestProvidedInterface, ApamComponent {
 
-	@SuppressWarnings("unused")
-	private void start() {
-		for (DependencyTestInterface dependency : dependencies) {
-			dependency.hello();
-		}
+    private List<DependencyTestInterface> dependencies;
 
-	}
+    public void apamStart(ASMInst apamInstance) {
+        System.out.println("Starting DependencyTest " + apamInstance);
+        for (DependencyTestInterface dependency : dependencies) {
+            dependency.hello();
+        }
+        System.out.println("Ending DependencyTest");
+    }
+
+    public void apamStop() {
+
+    }
+
+    public void apamRelease() {
+
+    }
 }
