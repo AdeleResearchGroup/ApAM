61dba2b1c1db66b9089af9a491575158942bb037
Jacky Estublier <Jacky.Estublier@imag.fr>
2011-11-07 10:30:58 +0000

    Share project "perfAPAM" into "http://henry.imag.fr/svn/apam-repos"

---
+++ APAM-Tests/perfAPAM/src/main/java/fr/imag/adele/apam/perfAPAM/ComponentTestImpl.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright Universite Joseph Fourier (www.ujf-grenoble.fr)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.imag.adele.apam.perfAPAM;
+
+import java.util.Properties;
+import java.util.Set;
+import java.util.UUID;
+
+import org.apache.felix.ipojo.ComponentInstance;
+import org.apache.felix.ipojo.Factory;
+import org.apache.felix.ipojo.InstanceManager;
+
+public class ComponentTestImpl implements Service {
+
+    public static int          instances = 0;
+    public static Set<Integer> checkpoints;
+    public static long         startTime;
+    private final int          miliers;
+
+    private Service            instance1;
+    private Service            instance2;
+
+    public ComponentTestImpl() {
+        ComponentTestImpl.instances++;
+        miliers = ComponentTestImpl.instances / 1000;
+        if (miliers * 1000 == ComponentTestImpl.instances) {
+//        if (ComponentTestImpl.checkpoints.contains(Integer.valueOf(ComponentTestImpl.instances))) {
+            Runtime r = Runtime.getRuntime();
+            r.gc();
+            long usedMemory = r.totalMemory() - r.freeMemory();
+            usedMemory = usedMemory / 1024;
+            usedMemory = usedMemory / 1024;
+            long duration = (System.nanoTime() - ComponentTestImpl.startTime) / 1000000;
+            System.out.println("NbInstances " + ComponentTestImpl.instances +
+                    " / usedMemory: " + usedMemory + "; duration (mili sec): " + duration);
+        }
+    }
+
+    @Override
+    public void call(int level) {
+        if (level == 0)
+            return;
+        instance1.call(level - 1);
+        instance2.call(level - 1);
+    }
+
+}
+/*    
+    public void start() {
+        if (currentLevel == null) {
+            throw new NullPointerException("currentLevel is null");
+        }
+        int i = Integer.valueOf(currentLevel).intValue();
+        if (i <= ComponentTestImpl.limit) {
+            try {
+                i++;
+                System.out.println("current level = " + currentLevel);
+                Properties props = new Properties();
+                props.put(ComponentTestImpl.CURRENTLEVELPROPERTY, Integer.valueOf(i).toString());
+                props.put("instance.name", UUID.randomUUID().toString());
+
+                ComponentInstance instanceRef = ComponentTestImpl.factory.createComponentInstance(props);
+                instanceRef.start();
+                instance1 = (ComponentTestImpl) ((InstanceManager) instanceRef).getPojoObject();
+
+                props.put("instance.name", UUID.randomUUID().toString());
+                instanceRef = ComponentTestImpl.factory.createComponentInstance(props);
+                instanceRef.start();
+                instance2 = (ComponentTestImpl) ((InstanceManager) instanceRef).getPojoObject();
+
+                props = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            notEnd = false;
+        }
+    }
+
+    public void stop() {
+        instance1 = null;
+        instance2 = null;
+    }
+
+    @Override
+    public void call() {
+        if (notEnd) {
+            instance1.call();
+            instance2.call();
+        }
+
+    }
+}
+*/
---
+++ APAM-Tests/perfAPAM/src/main/java/fr/imag/adele/apam/perfAPAM/Injector.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright Universite Joseph Fourier (www.ujf-grenoble.fr)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.imag.adele.apam.perfAPAM;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import fr.imag.adele.apam.CompositeTypeImpl;
+import fr.imag.adele.apam.apamAPI.ASMInst;
+import fr.imag.adele.apam.apamAPI.Apam;
+//import fr.imag.adele.apam.apamAPI.ApamComponent;
+import fr.imag.adele.apam.apamAPI.CompositeType;
+import fr.imag.adele.apam.perfAPAM.ComponentTestImpl;
+
+public class Injector implements Runnable, intTestApam {
+
+    Apam                        apam;
+    private static final String targetedNbInstance = "nbInstance";
+    int                         limit;
+
+    public void start() {
+        new Thread(this, "APAM perf test").start();
+    }
+
+    @Override
+    public void run() {
+        // public void apamStart() {
+        System.out.println("starting injector");
+        Integer[] temp = new Integer[] { Integer.valueOf(10),
+                Integer.valueOf(50), Integer.valueOf(100),
+                Integer.valueOf(250), Integer.valueOf(500),
+                Integer.valueOf(1000), Integer.valueOf(2000),
+                Integer.valueOf(3000), Integer.valueOf(4000),
+                Integer.valueOf(5000), Integer.valueOf(6000),
+                Integer.valueOf(7000), Integer.valueOf(8000),
+                Integer.valueOf(9000), Integer.valueOf(10000),
+                Integer.valueOf(11000), Integer.valueOf(12000),
+                Integer.valueOf(13000), Integer.valueOf(14000),
+                Integer.valueOf(15000), Integer.valueOf(16000),
+                Integer.valueOf(17000), Integer.valueOf(18000),
+                Integer.valueOf(19000), Integer.valueOf(20000)
+        };
+        ComponentTestImpl.checkpoints = new HashSet(Arrays.asList(temp));
+
+        JFileChooser fileChooser = new JFileChooser();
+        FileFilter filter = new FileNameExtensionFilter("*.properties",
+                new String[] { "properties" });
+        fileChooser.setFileFilter(filter);
+        int returnVal = fileChooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            Properties properties = new Properties();
+            try {
+                InputStream inputStream = (file.toURI()).toURL().openStream();
+                properties.load(inputStream);
+                inputStream.close();
+                String targeted = (String) properties
+                        .get(Injector.targetedNbInstance);
+                System.out.println("nb instances = " + targeted);
+                limit = Integer.valueOf(targeted).intValue();
+
+                createTreeInstance();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void createTreeInstance() {
+        System.out.println("create tree intance; limit: " + limit);
+        CompositeType testPerf = apam.createCompositeType("TestPerfApam", "TestApam",
+                    null /* models */, null /* properties */);
+        ASMInst test = testPerf.createInst(null /* composite */, null/* properties */);
+        Service s = (Service) test.getServiceObject();
+
+        long nanoTimeStart;
+        ComponentTestImpl.startTime = System.nanoTime();
+
+        for (int i = 1; i < 10; i++) {
+            nanoTimeStart = System.nanoTime();
+            s.call(limit);
+            long during = (System.nanoTime() - nanoTimeStart) / 1000;
+            System.out.println("Instances " + ComponentTestImpl.instances
+                        + " / invocation time (µS): " + during);
+        }
+    }
+
+//    private int calculateI(double x) {
+//        double ret = 0;
+//        ret = Math.log10((x + 1) * 0.5) / Math.log10(2);
+//
+//        return Double.valueOf(ret).intValue();
+//    }
+//
+//    @Override
+//    public void apamRelease() {
+//        // TODO Auto-generated method stub
+//
+//    }
+}
