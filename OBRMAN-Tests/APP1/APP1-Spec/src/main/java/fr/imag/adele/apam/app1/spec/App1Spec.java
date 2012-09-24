5cb77773d70b00a05bd6023eab347ad6a179aac7
mehdi-damou <mehdidamou@gmail.com>
2012-09-24 14:22:08 +0200

    refactoring test code

--- OBRMAN-Tests/APP1/APP1-Spec/src/main/java/fr/imag/adele/apam/app1/spec/App1Spec.java
+++
@@ -1,7 +0,0 @@
-package fr.imag.adele.apam.app1.spec;
-
-public interface App1Spec {
-
-    public void call(String texte);
-
-}
--- OBRMAN-Tests/APP2/APP2-Spec/src/main/java/fr/imag/adele/apam/app2/spec/App2Spec.java
+++
@@ -1,5 +0,0 @@
-package fr.imag.adele.apam.app2.spec;
-
-public interface App2Spec {
-    public void call(String texte);
-}
