7246a1fa812ffae5194af5e6f470acef72a1a060
Jacky Estublier <jacky.estublier@imag.fr>
2012-09-24 09:14:55 +0200

    Jeux de test sur dependance

--- APAMProject/ApamCore/src/main/java/fr/imag/adele/apam/core/DependencyDeclaration.java
+++ APAMProject/ApamCore/src/main/java/fr/imag/adele/apam/core/DependencyDeclaration.java
@@ -107,7 +107,7 @@ public class DependencyDeclaration extends TargetDeclaration {
     /**
      * The multiplicity of a dependency.
      * 
-     * If there are is calculated from the declaration of injected fields.
+     * If there are calculated from the declaration of injected fields.
      * 
      * If this is an abstract declaration in specifications or composites, it must be
      * explicitly defined.
