75bc7480fb2b378b1822ad13c813b35651b019cf
jandernascimento <jbotnascimento@gmail.com>
2012-12-11 13:51:53 +0100

    changing wait time for 0 to see how jenkins reacts

--- tests/helpers/src/main/java/fr/imag/adele/apam/tests/helpers/Constants.java
+++ tests/helpers/src/main/java/fr/imag/adele/apam/tests/helpers/Constants.java
@@ -2,7 +2,7 @@ package fr.imag.adele.apam.tests.helpers;
 
 public class Constants {
 
-	public static final int CONST_WAIT_TIME = 5000;
+	public static final int CONST_WAIT_TIME = 0;
 	
 	public static final int CONST_DEBUG_PORT = 5007;
 	
