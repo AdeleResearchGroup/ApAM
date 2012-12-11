54235ce39a8d7fb7cfd94af32476959b35592a40
Jacky Estublier <jacky.estublier@imag.fr>
2012-12-11 18:25:27 +0100

    Classe Resolved

---
+++ runtime/core/src/main/java/fr/imag/adele/apam/Resolved.java
@@ -0,0 +1,14 @@
+package fr.imag.adele.apam;
+
+import java.util.Set;
+
+public class Resolved {
+
+	public Set<Implementation> implementations ;
+	public Set<Instance> instances ;
+	
+	public Resolved (Set<Implementation> implementations, Set<Instance> instances ) {
+		this.implementations = implementations ;
+		this.instances = instances ;
+	}
+}
