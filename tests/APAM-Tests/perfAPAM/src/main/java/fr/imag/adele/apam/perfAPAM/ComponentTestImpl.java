e2de50c18fd8463f3318854ea2339117ae878c3c
Elmehdi Damou <Elmehdi.Damou@imag.fr>
2012-12-24 18:58:24 +0100

    Adding license to files - use maven-license-plugin

--- tests/APAM-Tests/perfAPAM/src/main/java/fr/imag/adele/apam/perfAPAM/ComponentTestImpl.java
+++ tests/APAM-Tests/perfAPAM/src/main/java/fr/imag/adele/apam/perfAPAM/ComponentTestImpl.java
@@ -1,16 +1,16 @@
 /**
- * Copyright Universite Joseph Fourier (www.ujf-grenoble.fr)
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE team
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package fr.imag.adele.apam.perfAPAM;
 
@@ -68,7 +68,7 @@ public class ComponentTestImpl implements Service {
                 testPerf.callPerf(1);
             }
             long during = (System.nanoTime() - nanoTimeStart) / 1000;
-            System.out.println(i * 1000 + "  calls loop. Invocation time (µS): " + during);
+            System.out.println(i * 1000 + "  calls loop. Invocation time (ï¿½S): " + during);
         }
     }
 
