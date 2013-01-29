31546f435f92d824b325ce18bfe47e5d31ba30ca
Jacky <jacky@imag.fr>
2013-01-29 17:22:18 +0100

    class Manager

---
+++ runtime/core/src/main/java/fr/imag/adele/apam/Manager.java
@@ -0,0 +1,27 @@
+package fr.imag.adele.apam;
+
+public interface Manager {
+	
+	/**
+	 * 
+	 * @return the name of that manager.
+	 */
+	public String getName();
+
+	/**
+	 *  returns the relative priority of that manager, for the resolution algorithm
+	 * @return
+	 */
+	public int getPriority();
+
+	/**
+	 * A new composite, holding a model managed by this manager, has been created. The manager is supposed to read and
+	 * interpret that model.
+	 * 
+	 * @param model the model.
+	 * @param composite the new composite (or appli)
+	 */
+	public void newComposite(ManagerModel model, CompositeType composite);
+
+
+}
