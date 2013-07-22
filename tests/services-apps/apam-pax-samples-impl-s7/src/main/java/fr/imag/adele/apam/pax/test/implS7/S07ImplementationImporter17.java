712056febabfaa196963890f1720d35377b5b7e0
jandernascimento <jbotnascimento@gmail.com>
2013-07-22 11:37:57 +0200

    adding missing file for the test

---
+++ tests/services-apps/apam-pax-samples-impl-s7/src/main/java/fr/imag/adele/apam/pax/test/implS7/S07ImplementationImporter17.java
@@ -0,0 +1,17 @@
+package fr.imag.adele.apam.pax.test.implS7;
+
+
+public class S07ImplementationImporter17 implements S07Interface17 {
+
+	public S07Dependency injected;
+
+	@Override
+	public String whoami() {
+		return "";
+	}
+
+	public S07Dependency getInjected() {
+		return injected;
+	}
+
+}
