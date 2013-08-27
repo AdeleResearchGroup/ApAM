225a89e166e17f195c24730ed6118853c35ceef5
jandernascimento <jbotnascimento@gmail.com>
2013-08-27 14:36:46 +0200

    adding tc121

---
+++ tests/services-apps/apam-pax-samples-impl-s7/src/main/java/fr/imag/adele/apam/pax/test/implS7/S07ImplementationImporter19.java
@@ -0,0 +1,19 @@
+package fr.imag.adele.apam.pax.test.implS7;
+
+import fr.imag.adele.apam.Specification;
+
+
+public class S07ImplementationImporter19 implements S07Interface19{
+
+	public Specification injected;
+
+	@Override
+	public String whoami() {
+		return "";
+	}
+
+	public Specification getInjected() {
+		return injected;
+	}
+
+}
---
+++ tests/services-apps/apam-pax-samples-impl-s7/src/main/java/fr/imag/adele/apam/pax/test/implS7/S07Interface19.java
@@ -0,0 +1,7 @@
+package fr.imag.adele.apam.pax.test.implS7;
+
+public interface S07Interface19 {
+
+	public String whoami() throws Exception;
+	
+}
