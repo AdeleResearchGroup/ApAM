9e11d21b8042a492bbdceb296b4c9eb3b2be2b5a
Thibaud Flury <thibaud.flury@imag.fr>
2013-09-17 17:40:16 +0200

    Added a simple gogo version of the shell commands

---
+++ tools/apam-gogo-shell/src/main/java/fr/imag/adele/apam/command/gogo/AsyncFind.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE team
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package fr.imag.adele.apam.command.gogo;
+
+import fr.imag.adele.apam.CST;
+import fr.imag.adele.apam.Component;
+import fr.imag.adele.apam.Composite;
+import fr.imag.adele.apam.Implementation;
+import fr.imag.adele.apam.Specification;
+import fr.imag.adele.apam.util.Util;
+
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AsyncFind implements Runnable {
+
+	private String componentName;
+
+	private PrintStream out;
+
+	private Composite target;
+
+	private boolean instantiate;
+	private String[] params ;
+	private Map<String, String> props ;
+
+	public AsyncFind(PrintStream out, Composite target, String componentName, boolean b, String[] params) {
+		this.out= out;
+		this.target = target;
+		this.componentName = componentName;
+		this.instantiate = b;
+		this.params = params ;
+	}
+
+
+
+	@Override
+	public void run() {
+		Component  component= CST.apamResolver.findComponentByName(target, componentName);
+		if (component == null) {
+			System.out.println("Component " + componentName + " not found.");
+			return ;
+		}
+
+		if (params != null && params.length > 1) {
+			props = new HashMap <String, String> () ;
+			String value = "";
+			//Skip first values, which is the component name
+			for (int i = 1 ; i < params.length ;i++) {
+				value +=  params[i] + ", " ;
+			}
+			props.put ("param", value) ; 
+		} else props = null ;
+
+		if (instantiate){
+			if (component instanceof Implementation)
+				((Implementation)component).createInstance(target, props);
+			if (component instanceof Specification) {
+				Implementation impl = CST.apamResolver.resolveSpecByName(target, componentName, null, null) ;
+				if (impl == null) {
+					System.out.println("Component " + componentName + " not found.");
+					return ;
+				}
+				impl.createInstance(null, props);
+			}
+		}
+	}
+}
