e0a4ba194cf60ac92e6b64b00e063102cb06e915
vega <german.vega@imag.fr>
2014-07-23 08:45:34 +0200

    Open The Box : update scenario for final review

--- simulation/security/src/main/java/fr/imag/adele/apam/application/emergency/EmergencyDetection.java
+++ simulation/security/src/main/java/fr/imag/adele/apam/application/emergency/EmergencyDetection.java
@@ -14,8 +14,11 @@
  */
 package fr.imag.adele.apam.application.emergency;
 
+import fr.imag.adele.apam.ApamManagers;
+import fr.imag.adele.apam.Component;
 import fr.imag.adele.apam.Instance;
-import fr.liglab.adele.apam.device.fire.EmergencyEvent;
+import fr.imag.adele.apam.PropertyManager;
+//import fr.liglab.adele.apam.device.fire.EmergencyEvent;
 
 /**
  * This class keeps track of the current state of the fire management domain.
@@ -25,7 +28,7 @@ import fr.liglab.adele.apam.device.fire.EmergencyEvent;
  * @author vega
  *
  */
-public class EmergencyDetection {
+public class EmergencyDetection implements PropertyManager {
 
 	/**
 	 * The identified emergency states in the domain
@@ -70,32 +73,74 @@ public class EmergencyDetection {
 	@SuppressWarnings("unused")
 	private void start(Instance instance) {
 		this.instance = instance;
+		ApamManagers.addPropertyManager(this);
+
 	}
 	
 	/**
 	 * Evaluate the state of the fire management domain, from different notifications
 	 * coming from the enclosed applications 
 	 */
-	@SuppressWarnings("unused")
-	private void onEvent(EmergencyEvent event) {
+//	@SuppressWarnings("unused")
+//	private void onEvent(EmergencyEvent event) {
+//		setState(event.isEmergency());
+//	}
+
+
+	private void setState(boolean onFire) {
 		switch (state) {
 		
 		case NORMAL:
-			if (event.isEmergency()) {
+			if (onFire) {
 				state = State.EMERGENCY;
 			}
 			break;
 			
 		case EMERGENCY:
-			if (!event.isEmergency()) {
+			if (!onFire) {
 				state = State.NORMAL;
 			}
 			break;
 		}
-		
+
 		stateProperty = state.getLabel();
 		instance.getComposite().setProperty("emergency",stateProperty);
+		
+	}
+
+	@Override
+	public void attributeChanged(Component component, String attr, String newValue, String oldValue) {
+		
+		System.out.println("emergency detection : attribute changed  "+attr+" ="+newValue+" component "+component+ " " +component.getClass().getName());
+		
+		if (!(component instanceof Instance))
+			return;
+		
+		Instance source = (Instance) component;
+		
+		System.out.println("emergency detection : component  "+component+" in composite ="+source.getComposite());
+		System.out.println("emergency detection : my composite  "+this.instance.getComposite());
+		
+		if (source.getComposite().equals(this.instance.getComposite()) && attr.equals("onFire")) {
+			System.out.println("emergency detected, onfire ="+newValue);
+			setState(newValue.equalsIgnoreCase("true"));
+		} 
+
+	}
+
+	@Override
+	public void attributeAdded(Component component, String attr, String newValue) {
+		attributeChanged(component, attr, newValue, null);
+	}
+
+	@Override
+	public void attributeRemoved(Component component, String attr,	String oldValue) {
+		attributeChanged(component, attr, null, oldValue);
 	}
 
 
+	@Override
+	public String getName() {
+		return "EmergencyDetection";
+	}
 }
